b9543b82ad1e5410f3df9659a39209fb270a1047
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-07-03 12:08:40 +0200

    snack 1 complete

--- snack1.test.js
+++ snack1.test.js
@@ -2,7 +2,11 @@
 
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
 
+function getInitials(nome, cognome) {
+  return nome.charAt(0) + cognome.charAt(0);
+}
+
 test('La funzione "getInitials" restituisce le iniziali di un nome completo', () => {
-  expect(getInitials('Claudia', 'Destris')).toBe('C', 'D');
-  expect(getInitials('Francesco', 'Crisco')).toBe('F', 'C');
+  expect(getInitials('Claudia', 'Destris')).toBe('CD');
+  expect(getInitials('Francesco', 'Crisco')).toBe('FC');
 });
